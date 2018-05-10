7e9d531bc5493a976704aebb5118a78afc11c066
peng <pc175@uow.edu.au>
2018-05-10 18:31:43 -0400

    introduce optimize_a_cmd, still not working

--- controller.py
+++ controller.py
@@ -19,6 +19,8 @@ MOI = np.array([0.005, 0.005, 0.01])
 MAX_THRUST = 10.0
 MAX_TORQUE = 1.0
 
+np.set_printoptions(suppress=True) # disable scientific notation
+
 
 class NonlinearController(object):
 
@@ -41,7 +43,6 @@ class NonlinearController(object):
 
         ind_min = np.argmin(np.abs(np.array(time_trajectory) - current_time))
         time_ref = time_trajectory[ind_min]
-
 
         if current_time < time_ref:
             position0 = position_trajectory[ind_min - 1]
@@ -69,10 +70,9 @@ class NonlinearController(object):
         position_cmd = (position1 - position0) * \
                        (current_time - time0) / (time1 - time0) + position0
         velocity_cmd = (position1 - position0) / (time1 - time0)
-        
-        
+
         return position_cmd, velocity_cmd, yaw_cmd
-    
+
     def lateral_position_control(self, local_position_cmd, local_velocity_cmd, local_position, local_velocity,
                                  acceleration_ff = np.array([0.0, 0.0])):
         """Generate horizontal acceleration commands for the vehicle in the local frame
@@ -105,7 +105,7 @@ class NonlinearController(object):
         drone's mass accordingly!
         """
         return 0.0
-        
+
 
     def roll_pitch_controller(self, acceleration_cmd, attitude, thrust_cmd):
         """ Generate the rollrate and pitchrate commands in the body frame
@@ -158,15 +158,21 @@ class PID(object):
     D: float = 0.0
 
 
+MAX_THRUST_A = MAX_THRUST / DRONE_MASS_KG
+MAX_THRUST_A_SQ = MAX_THRUST_A**2
 @dataclass
 class ControllerImpl(NonlinearController):
-    torque: PID = PID(0.2)  # just P
-    atti: PID = PID(0.5)  # just P
-    position: PID = PID(0.5, 0, 0.1)
+    torque: PID = PID(0.1)  # just P
+    atti: PID = PID(0.4)  # just P
+    position: PID = PID(0.4, 0, - 0.1)
+
+    itrAtti: int = 0
 
     def __post_init__(self):
-        self.intg_xy = np.array([0, 0])
-        self.intg_z = 0
+        self.MAX_THRUST_A = MAX_THRUST / DRONE_MASS_KG
+
+        self.intg_dxy = np.array([0.0, 0.0])
+        self.intg_dz = 0.0
 
     def lateral_position_control(self, local_position_cmd, local_velocity_cmd, local_position, local_velocity,
                                  acceleration_ff=np.array([0.0, 0.0])):
@@ -181,18 +187,18 @@ class ControllerImpl(NonlinearController):
 
         Returns: desired vehicle 2D acceleration in the local frame [north, east]
         """
-        return np.array([0,0])
+        # return np.array([0,0])
 
-        # d_xy = local_position_cmd - local_position
-        # self.intg_xy += d_xy
-        # i_xy = self.intg_xy
-        # d_dot_xy = local_velocity_cmd - local_velocity
-        #
-        # pid = self.position
-        # dot_v = pid.P * d_xy + pid.I * i_xy + pid.D * d_dot_xy + acceleration_ff
-        #
-        # output = dot_v
-        # return output
+        dxy = local_position_cmd - local_position
+        self.intg_dxy += dxy
+        ixy = self.intg_dxy
+        d_dot_xy = local_velocity_cmd - local_velocity
+
+        pid = self.position
+        dot_v = pid.P * dxy + pid.I * ixy + pid.D * d_dot_xy + acceleration_ff
+
+        output = dot_v
+        return output
 
 
     def altitude_control(self, altitude_cmd, vertical_velocity_cmd, altitude, vertical_velocity, attitude,
@@ -211,8 +217,8 @@ class ControllerImpl(NonlinearController):
         """
 
         d_z = altitude_cmd - altitude
-        self.intg_z += d_z
-        i_z = self.intg_z
+        self.intg_dz += d_z
+        i_z = self.intg_dz
         d_dot_z = vertical_velocity_cmd - vertical_velocity
 
         pid = self.position
@@ -229,6 +235,7 @@ class ControllerImpl(NonlinearController):
         clamped = min(MAX_THRUST, max(0, output))
         return clamped
 
+
     def roll_pitch_controller(self, acceleration_cmd, attitude, thrust_cmd):
         """ Generate the rollrate and pitchrate commands in the body frame
 
@@ -239,39 +246,93 @@ class ControllerImpl(NonlinearController):
 
         Returns: 2-element numpy array, desired rollrate (p) and pitchrate (q) commands in radians/s
         """
-        q_current = Quaternion(list(attitude))  # xyz
-
-        ma_xy = DRONE_MASS_KG * acceleration_cmd
-        ma_z = - math.sqrt(thrust_cmd**2 - np.inner(ma_xy, ma_xy))
-        ma = np.array([*ma_xy, ma_z])
-
-        # test
-        # ma_norm = np.linalg.norm(ma)
-        # assert(ma_norm == thrust_cmd)
-
-        f_body = np.array([0, 0, -thrust_cmd])
-        q_target = self.getRotationQuaternion(f_body, ma)
-
-        d_q = q_target * q_current.inversed
-        d_q.normalize()
         try:
+            q_current = Quaternion(list(attitude))
+            R_current = q_current.dcm
+            R33 = R_current.c.z
+
+            thrust_a = thrust_cmd / DRONE_MASS_KG
+            thrust_a_z = thrust_a * R33
+
+            a_cmd = np.array([*acceleration_cmd, - thrust_a_z])
+            a_cmd_optimized = self.optimize_a_cmd(a_cmd, attitude, thrust_a)
+
+            a_body = np.array([0, 0, -thrust_a])
+            q_target = self.getRotationQuaternion(a_body, a_cmd_optimized)
+
+            d_q = q_target * q_current.inversed
+            d_q.normalize()
             d_angle = d_q.euler
+
+            print(
+                # str(q_current.euler), "\t==",
+                q_target.euler, "\t=>", a_cmd)
+            a_cmd
+
         except AssertionError as ee:
             raise ee
 
         d_angle_xy = d_angle[[0, 1]]
+        self.itrAtti += 1
         return d_angle_xy * self.atti.P
+
+    def optimize_a_cmd(self, a_cmd, attitude, thrust_a):
+        """
+        :param a_cmd: target acceleration excluding gravity
+        :param thrust_a: current thrust-induced-acceleration excluding gravity
+        :return: optimized target acceleration within flight envelop
+        avoid flipping / downward thrust
+        if max thrust is under powered, attempt to maintain z first
+
+        theoretically should only use a_cmd, other args are just for show
+
+        """
+        info = [
+            "itr_attitude:", self.itrAtti,
+            "\tThrust-induced-acceleration:", thrust_a
+        ]
+
+        result = a_cmd.copy()
+        if result[2] >= 0:
+            result[2] = 0
+
+        a_cmd_L2Sq = np.dot(result, result)
+
+        if a_cmd_L2Sq > MAX_THRUST_A_SQ:
+            info.extend([
+                "\tunderpowered:", MAX_THRUST_A_SQ - a_cmd_L2Sq,
+                "\tunable to achieve such acceleration! slowing down"
+            ])
+            max_xyL2Sq = MAX_THRUST_A_SQ - result[2]**2
+            if max_xyL2Sq < 0:
+                result[:] = [0, 0, - MAX_THRUST_A]
+            else:
+                xy_cmd = result[[0,1]]
+                xy_L2Sq = np.dot(xy_cmd, xy_cmd)
+                ratio = math.sqrt(max_xyL2Sq / xy_L2Sq)
+                result[[0,1]] = result[[0,1]] * ratio
+
+                # assert np.dot(result, result) == MAX_THRUST_A_SQ, str(np.dot(result, result)) + " != " + str(MAX_THRUST_A_SQ)
+
+
+        print(*info)
+        return result
 
     @staticmethod
     def getRotationQuaternion(v1: np.array, v2: np.array) -> Quaternion:
         """v1 and v2 are xyz vectors"""
-        if np.array_equal(v1, v2):
+        if np.dot(v1, v1) == 0 or np.dot(v2, v2) == 0 or np.array_equal(v1, v2):
             return Quaternion([1, 0, 0, 0])
 
         cross = np.cross(v1, v2)
         residual = math.sqrt(np.dot(v1, v1) * np.dot(v2, v2)) + np.dot(v1, v2)
         q_target = Quaternion(list([residual, *cross]))
         q_target.normalize()
+
+        # try:
+        #     q_target.dcm
+        # except Exception as ee:
+        #     raise ee
 
         # test
         # d_R = Matrix3()
@@ -307,5 +368,10 @@ class ControllerImpl(NonlinearController):
         d_body_rate = (body_rate_cmd - body_rate) * self.torque.P
         jj = np.diag(MOI)
         torque = d_body_rate + np.cross(body_rate, np.matmul(jj, body_rate))
+        torque_L2 = np.linalg.norm(torque)
+
+        if torque_L2 >= MAX_TORQUE:
+            torque =  (MAX_TORQUE / torque_L2) * torque
+            assert(np.linalg.norm(torque) - 1 <= 0.00000001)
 
         return torque
